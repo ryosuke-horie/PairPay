a5ac6a8a61e1a2246ecb4814ebb56b09b57c2c5b
ryosuke-horie <r_horie@sincereed.com>
2025-01-18 11:03:55 +0900

    パスワードを追加

--- backend/drizzle/schema.ts
+++ backend/drizzle/schema.ts
@@ -6,6 +6,7 @@ export const users = sqliteTable('users', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
+  password: text('password').notNull(),
   createdAt: integer('created_at', { mode: 'timestamp' }).notNull().default(sql`CURRENT_TIMESTAMP`),
   updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull().default(sql`CURRENT_TIMESTAMP`),
 });
